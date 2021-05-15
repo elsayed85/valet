242e46dd5df12703ae857ca531a9155cee147284
= <=>
2021-05-15 13:14:41 +0200

    “wip”

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,40 @@
 <?php
 
+use App\Valet\Valet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('sites', function () {
+    $sites = (new  Valet())->allSites();
+    return response()->json($sites->toArray());
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('logs', function () {
+    $logs = (new  Valet())->logs();
+    return response()->json($logs);
+});
+
+Route::get('logs/show', function (Request $request) {
+    $log = (new  Valet())->loadLogFile($request->path, $request->take, $request->skip);
+    return response()->json($log);
+});
+
+Route::get('on-latest-version', function (Request $request) {
+    $onLatestVerision = (new  Valet())->onLatestVerision();
+    return response()->json(['onLatestVerision' => $onLatestVerision]);
+});
+
+Route::get('run', function () {
+    $list = (new  Valet())->run();
+    return response()->json($list);
+});
+
+Route::post('link', function (Request $request) {
+    $link = (new  Valet())->link($request->path, $request->name)->save();
+    return response()->json($link);
+});
+
+Route::post('unlink', function (Request $request) {
+    $done = (new  Valet())->unlink($request->name);
+    return response()->json(['status' => $done]);
 });
